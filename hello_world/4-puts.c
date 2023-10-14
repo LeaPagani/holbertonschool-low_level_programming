b5396a11660a5aaf6559fa3b53e01cad94699c17
LeaPagani <leandropaganinieves@gmail.com>
2023-10-14 13:38:47 -0400

    Puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,8 @@
+/* Print text program */
 #include <stdio.h>
 
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+int main(void) 
+{
+    puts("Programming is like building a multilingual puzzle\n");
+    return(0);
 }
-
