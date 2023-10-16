a264a90afdd88c12d29f6ec3bb1f452c4af8100f
LeaPagani <leandropaganinieves@gmail.com>
2023-10-16 12:52:34 -0400

    Determine if a number is positive or negatative

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 /* more headers goes there */
 
@@ -15,13 +16,13 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
     /* Check if n is positive */
 	if (n > 0)
-		printf("%d is positive\n");
+		printf("%d is positive\n", n);
     /* Check if n is negative */
 	else if (n < 0)
-		printf("%d is negative\n");
+		printf("%d is negative\n", n);
     /* Check if n is zero */
 	else
-		printf("%d is zero\n");
+		printf("%d is zero\n", n);
 	return (0);
 }
 
