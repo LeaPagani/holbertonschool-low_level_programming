7529b270216c53e5e400f0142c7bac49a1a46a7f
LeaPagani <leandropaganinieves@gmail.com>
2023-10-17 14:24:58 -0400

    Print _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+*main - Write a program that prints _putchar, followed by a new line
+*
+*Return: 0
+*/
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
