ecf3a8235557cf137badee5719f0a8f7afe55de4
LeaPagani <leandropaganinieves@gmail.com>
2023-10-17 09:31:09 -0400

    Print the uppercase alphabet with putchar

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+*main - print the alphabet
+*
+*Return: 0
+*/
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar (c);
+	for (c = 'A'; c <= 'Z'; c++)
+		putchar (c);
+	putchar ('\n');
+
+	return (0);
+
+}
