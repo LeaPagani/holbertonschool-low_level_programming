9f85c2158d0ccae3824ca84cb014fc1d5c188add
LeaPagani <leandropaganinieves@gmail.com>
2023-10-23 21:56:35 -0400

    Function that returns the length of a string

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Leandro Pagani
+ */
+
+#include "main.h"
+
+/**
+* _strlen - Function that returns the length of a string
+* @s: String
+* Return: 0
+*/
+int _strlen(char *s)
+{
+	int length;
+
+	while (s[length} != '\0');
+	{
+	length++;
+	}
+
+	return (length);
+}
