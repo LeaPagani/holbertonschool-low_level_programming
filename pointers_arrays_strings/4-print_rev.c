83e7c3fadb97afbbff1bf8cc7a59566d268dd843
LeaPagani <leandropaganinieves@gmail.com>
2023-10-24 10:39:54 -0400

    Function that prints a string in reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,19 +1,15 @@
-/**
-* File: 4-print_rev.c
-* Auth: Leandro Pagani
-*/
-
 #include "main.h"
 
 /**
 * print_rev - Function that prints a string in reverse followed by a new line
 * @s: String
-* Return: 0
 */
 void print_rev(char *s)
 {
 	while (*s)
+	{
 		s++;
+	}
 
 	s--;
 
