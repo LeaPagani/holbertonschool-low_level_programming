48c9c49adddf3afe0a01ec505f8687d6f7829583
LeaPagani <leandropaganinieves@gmail.com>
2023-10-24 20:18:27 -0400

    Function that prints n elements of an array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,6 +3,7 @@
 
 /**
 * print_array - Function that prints n elements of an array
+* @a: Array
 * @n: Number of elements
 */
 void print_array(int *a, int n)
@@ -12,7 +13,7 @@ void print_array(int *a, int n)
 	if (n > 0)
 	{
 		printf("%d", a[0]);
-		for (x = 1; x >= n; x++)
+		for (x = 1; x < n; x++)
 		{
 			printf(", %d", a[x]);
 		}
