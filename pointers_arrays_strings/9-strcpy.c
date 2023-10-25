fb24991cacdbd806bc6b0a1511545be162db4717
LeaPagani <leandropaganinieves@gmail.com>
2023-10-24 22:54:29 -0400

    Function that copies a string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
-* _strcpy - Function that copies a string.
+* _strcpy - Function that copies a string
 * @dest: Destination string where the copy will be placed
 * @src: Source string that will be copied
+* Return: A pointer to the destination string `dest`
 */
 char *_strcpy(char *dest, char *src)
 {
