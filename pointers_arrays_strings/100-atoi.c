fa740355a5d1d9349565c2ffc4279fa515476206
LeaPagani <leandropaganinieves@gmail.com>
2023-10-25 10:13:04 -0400

    Function that convert a string to an integer

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+* _atoi - Function that convert a string to an integer
+* @s: String
+* Return: Int value
+*/
+int _atoi(char *s)
+{
+	int pointer, value, pn;
+
+	pointer = value = 0;
+
+	pn = 1;
+
+	while (s[pointer])
+	{
+		while ((s[pointer] < '0' || s[pointer] > '9') && (s[pointer] != '\0'))
+		{
+			if (s[pointer] == '-')
+			{
+				pn = -1;
+				pointer++;
+			}
+			else
+			{
+				pn = 1;
+				pointer++;
+			}
+		}
+		if ((s[pointer] >= '0') && (s[pointer] <= '9'))
+		{
+			while ((s[pointer] >= '0') && (s[pointer] <= '9'))
+			{
+				value = (value * 10) + ((s[pointer]) - '0');
+				pointer++;
+			}
+		}
+	}
+
+	value = value * pn;
+
+	return (value);
+}
