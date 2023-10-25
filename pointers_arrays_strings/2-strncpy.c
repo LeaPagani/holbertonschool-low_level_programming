0732293bee68ae4dda977e5bff588a2d1973bf8e
LeaPagani <leandropaganinieves@gmail.com>
2023-10-25 14:14:19 -0400

    Function that copies a string

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
-* _strncat - Function that concatenates two strings
+* _strncpy - Function that copies a string
 * @src: String 1
 * @dest: String 2
 * @n: Number of bytes
 * Return: Pointer to the resulting string dest
 */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	char *ogdest = dest;
 
-	while (*src)
+	while (*src && n > 0)
 	{
 		*dest = *src;
 		dest++;
 		src++;
+		n--;
 	}
 
 	*dest = '\0';
