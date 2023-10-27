999eb221bd63d0d6459be15fa73cd0e828f90cec
LeaPagani <leandropaganinieves@gmail.com>
2023-10-27 13:26:09 -0400

    Function that locates a character in a string

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* int _strspn - Function that gets the length of a prefix substring
+* @s: String
+* @c: Char to be located
+* Return: Pointer to first occurrence of c or NULL if c is not found
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	while (*s)
+	{
+		if ([*s] == c)
+				return (s);
+	{
+		else
+	{
+			*s++;
+	}
+	return ('\0');
+}
