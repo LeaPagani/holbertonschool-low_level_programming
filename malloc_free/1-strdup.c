18132ccf7ac5e4ab925c963ca7b366e42e3ee5eb
LeaPagani <leandropaganinieves@gmail.com>
2023-11-10 09:42:05 -0400

    Duplicate string with allocated memory

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* create_array - Function creates array of chars with a specific char
+* @size: Size of the array
+* @c: Specified char
+* Return: Array or NULL
+*/
+char *_strdup(char *str)
+{
+	char *duplicate;
+	unsigned int length;
+	unsigned int i;
+
+	length = 0;
+	i = 0;
+
+	if (str == NULL)
+		return NULL;
+
+	while (str[length] != '\0')
+		length++;
+
+	duplicate = (char *)malloc((length + 1) * sizeof(char));
+
+	if (duplicate == NULL)
+		return NULL;
+
+	while (i < length)
+	{
+		duplicate[i] = str[i];
+		i++;
+	}
+
+	duplicate[i] = '\0';
+
+	return duplicate;
+}
