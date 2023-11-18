7debdf80e35e33744541eb7cfbbc5dec4d3672fa
LeaPagani <leandropaganinieves@gmail.com>
2023-11-17 22:00:57 -0400

    Execute a function on each element of an array

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+* array_iterator - Function that executes a function on each element of an array
+* @array: Array of integer
+* @size: Size of array
+* @action: Pointer to a function
+**/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
