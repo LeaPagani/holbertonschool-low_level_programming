7230379fad6b7b49fed8526c0bb46142a843e34d
LeaPagani <leandropaganinieves@gmail.com>
2023-11-19 18:49:31 -0400

    Variadic Functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,95 +1,43 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-
 /**
-* print_char - Prints a char
-* @vargs: Argument
-* Return: 0
-*/
-void print_char(va_list vargs)
-{
-printf("%c", va_arg(vargs, int));
-}
-
-/**
-* print_integer - Prints an integer
-* @vargs: Argument
-* Return: 0
-*/
-void print_integer(va_list vargs)
-{
-printf("%d", va_arg(vargs, int));
-}
-
-/**
-* print_float - Prints a float
-* @vargs: Argument
-* Return: 0
-*/
-void print_float(va_list vargs)
-{
-printf("%f", va_arg(vargs, double));
-}
-
-/**
-* print_string - Prints a string
-* @vargs: Argument
-* Return: Void
-*/
-void print_string(va_list vargs)
-{
-char *ch;
-ch = va_arg(vargs, char *);
-if (ch == NULL)
-{
-printf("(nil)");
-return;
-}
-printf("%s", ch);
-}
-
-/**
-* print_all - Function prints all arguments
-* @format: Format
-* Return: 0
-*/
-
+ * print_all - Function that prints everything
+ * @format: Type of argument
+ */
 void print_all(const char * const format, ...)
 {
-int i, j;
-char *str = "";
-va_list args;
+	va_list object;
+	int i = 0;
+	char  *s;
 
-form p[] = {
-{"c", print_char},
-{"i", print_integer},
-{"f", print_float},
-{"s", print_string},
-{NULL, NULL},
-};
-
-
-va_start(args, format);
-
-i = 0;
-while (format && format[i])
-{
-j = 0;
-while (p[j].f)
-{
-if (*(p[j].f) == format[i])
-{
-printf("%s", str);
-p[j].func(args);
-str = ", ";
-}
-j++;
-}
-i++;
-}
-
-va_end(args);
-printf("\n");
+	va_start(object, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+			case 'c':
+				printf("%c", va_arg(object, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(object, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(object, double));
+				break;
+			case 's':
+				s = va_arg(object, char *);
+				s ? printf("%s", s) : printf("(nil)");
+				break;
+			}
+			if ((format[i] == 'c' || format[i] == 'i' || format[i] == 'f'
+						|| format[i] == 's') && format[i + 1])
+			{
+				printf(", ");
+			}
+			i++;
+		}
+		va_end(object);
+	}
+		printf("\n");
 }
